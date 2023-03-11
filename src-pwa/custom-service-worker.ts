f7ffd912dcfb5d8b1cd00336a958adde8d678ccd
ParticleG <particle_g@outlook.com>
2023-03-12 01:25:00 +0800

    - Init project

---
+++ src-pwa/custom-service-worker.ts
@@ -0,0 +1,56 @@
+/*
+ * This file (which will be your service worker)
+ * is picked up by the build system ONLY if
+ * quasar.config.js > pwa > workboxMode is set to "injectManifest"
+ */
+
+declare const self: ServiceWorkerGlobalScope & typeof globalThis;
+
+import { precacheAndRoute } from 'workbox-precaching';
+
+// Use with precache injection
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pubKey =
+  'BGzzMG2Z2ASNOXFg8S019mQNeSWD-V3dDY2BVTDTCUuZ300uyWBQCMMWTOLFPJibQqaLSBlv6f4lz3rVi8YFu30';
+
+self.registration.pushManager
+  .getSubscription()
+  .then((subscription) => {
+    if (subscription) {
+      return subscription;
+    }
+    return self.registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: pubKey,
+    });
+  })
+  .then(
+    (pushSubscription) => {
+      console.log(pushSubscription);
+      fetch('http://127.0.0.1:3000/subscribe', {
+        method: 'post',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          subscription: pushSubscription,
+        }),
+      })
+        .then((r) => console.log(r))
+        .catch((e) => console.log(e));
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
+
+self.addEventListener('push', function (event) {
+  console.log('On push event');
+  console.log(event);
+  event.waitUntil(
+    self.registration.showNotification('TEST', {
+      body: event.data ? event.data.text() : 'no payload',
+    })
+  );
+});
