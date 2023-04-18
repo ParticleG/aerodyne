a78ffdebe62cbbb502cfb3867c395831f9cc120e
ParticleG <particle_g@outlook.com>
2023-04-18 20:40:09 +0800

    - Try fixing mobile page height

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh LpR lFf">
+  <q-layout view="lHh LpR lFf" @resize="onResize">
     <router-view
       name="header"
       :mobile="screen.lt.md"
@@ -16,7 +16,7 @@
         class="scrollbar-page"
         :class="screen.lt.md ? 'q-px-md' : 'q-pl-md'"
         @toggle:drawer="toggleLeftDrawer"
-        style="height: calc(100vh - 122px)"
+        :style="{ height: `${pageHeight}px` }"
       />
     </q-page-container>
     <router-view name="footer" @toggle:drawer="toggleLeftDrawer" />
@@ -30,7 +30,12 @@ import { ref } from 'vue';
 const { screen } = useQuasar();
 const leftDrawerOpen = ref(true);
 
-function toggleLeftDrawer(action?: boolean) {
+const pageHeight = ref(0);
+
+const onResize = ({ height }) => {
+  pageHeight.value = height - 122;
+};
+const toggleLeftDrawer = (action?: boolean) => {
   leftDrawerOpen.value = action ?? !leftDrawerOpen.value;
-}
+};
 </script>
